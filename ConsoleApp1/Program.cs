8a2183d77814d1ab128a0434e8cf44407417a1ad
Àngel Delgado <adelgado@medysif.com>
2019-07-01 16:54:11 +0200

    01

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System.ServiceModel;
+using System.ServiceModel.Channels;
+using System.ServiceModel.Security;
+using System.ServiceModel.Security.Tokens;
 using System.Xml;
 using ConsoleApp1.DispatcherV2Signed;
 
@@ -20,10 +23,9 @@ namespace ConsoleApp1
             //certAuth.MapClientCertificateToWindowsAccount = false;
             //certAuth.RevocationMode = X509RevocationMode.Online;
             //certAuth.TrustedStoreLocation = StoreLocation.LocalMachine;
-            
-            var bindings = new WSHttpBinding(SecurityMode.TransportWithMessageCredential);
+
             var endpoint = new EndpointAddress("https://test.e-notario.notariado.org/dispatcher-web/DispatcherV2Signed");
-            var client = new DispatcherV2SignedClient(bindings, endpoint);
+            var client = new DispatcherV2SignedClient(GetCustomBinding(), endpoint);
 
             var header = new SERVICE_DISPATCHER();
             var xDoc = new XmlDocument();
@@ -31,5 +33,51 @@ namespace ConsoleApp1
 
             var response = client.process(header, body);
         }
+
+        private static Binding GetCustomBinding()
+        {
+            var httpsBindingElement = new HttpsTransportBindingElement
+            {
+                AllowCookies = false,
+                BypassProxyOnLocal = false,
+                HostNameComparisonMode = HostNameComparisonMode.StrongWildcard,
+                MaxBufferPoolSize = 524288,
+                MaxBufferSize = 65536,
+                MaxReceivedMessageSize = 65536,
+                RequireClientCertificate = true,
+                UseDefaultWebProxy = true
+            };
+
+            var asymmetricSecurityBindingElement = new AsymmetricSecurityBindingElement
+            {
+                InitiatorTokenParameters = new X509SecurityTokenParameters()
+                {
+                    X509ReferenceStyle = X509KeyIdentifierClauseType.Any,
+                    InclusionMode = SecurityTokenInclusionMode.AlwaysToInitiator
+
+                },
+                RecipientTokenParameters = new X509SecurityTokenParameters()
+                {
+                    X509ReferenceStyle = X509KeyIdentifierClauseType.Any,
+                    InclusionMode = SecurityTokenInclusionMode.AlwaysToRecipient
+                },
+                DefaultAlgorithmSuite = SecurityAlgorithmSuite.TripleDesRsa15,
+                SecurityHeaderLayout = SecurityHeaderLayout.Lax,
+                IncludeTimestamp = true,
+                MessageSecurityVersion = MessageSecurityVersion.WSSecurity10WSTrustFebruary2005WSSecureConversationFebruary2005WSSecurityPolicy11BasicSecurityProfile10
+            };
+
+            asymmetricSecurityBindingElement.EndpointSupportingTokenParameters.SignedEncrypted.Add(new X509SecurityTokenParameters());
+            asymmetricSecurityBindingElement.EndpointSupportingTokenParameters.SetKeyDerivation(false);
+
+            var myBinding = new CustomBinding();
+
+            myBinding.Elements.Add(asymmetricSecurityBindingElement);
+            myBinding.Elements.Add(new TextMessageEncodingBindingElement(MessageVersion.Soap11, System.Text.Encoding.UTF8));
+            myBinding.Elements.Add(httpsBindingElement);
+
+            return myBinding;
+        }
+
     }
 }
